00f9a0a99932f6c27d9ed372a81a70b5a0cee299
SahilSindhu <sahilsindhu16@gmail.com>
2019-11-18 23:57:42 +0530

    story 4 complete

--- js/movie-search-page/getMovieDB.js
+++ js/movie-search-page/getMovieDB.js
@@ -1,7 +1,4 @@
-import { loadMovieData } from '../app-common-functionalities/load-movie-data.js';
-import { api_urls } from '../app-common-functionalities/constants/api-urls.js';
 export function getMovieDB(){
-        
         let localMovieDb = JSON.parse(localStorage.getItem('localMovieDB'));
         let localGenreDb = JSON.parse(localStorage.getItem('localGenreDb'));
         if(localMovieDb != null){
@@ -14,21 +11,6 @@ export function getMovieDB(){
                 }
             });
         }
-
-        else{
-            // let homePageApi = [api_urls.LATEST_MOVIES_API,api_urls.POPULAR_MOVIE_API,api_urls.TRENDING_MOVIES_API];
-            // homePageApi.forEach((url,idx )=>{
-            //     loadMovieData(url).then(res => genre_promise.then(()=>{
-            //                 allData.push(res.results);
-            //                 if(allData.length == '3'){
-            //                     localStorage.setItem('localMovieDB',JSON.stringify(allData));
-            //                     localStorage.setItem('localGenreDb',JSON.stringify(genreData[0].genres))
-            //                 }
-            //             }
-            //     ));
-            // })
-        }
-        
         return [localMovieDb,localGenreDb]
 
 }
--- js/movie-search-page/movie-search.js
+++ js/movie-search-page/movie-search.js
@@ -4,26 +4,24 @@ import { movieQuickView } from '../app-common-functionalities/movie-popup.js';
 import{ insertTemplateMarkup } from '../app-common-functionalities/markup-templates.js';
 
 var querryField = document.querySelector('input');
-var rating_range = document.querySelector('.rating-range');
+var rating_range = document.querySelector('.rating__range');
 var rating =rating_range.value;
 var submitButton = document.querySelector('Button');
-var filtererdData =[];
+var filtererdData =new Array();
 
 var data = getMovieDB();
 var localMovieDb =data[0];
 var localGenreDb = data[1];
 
 insertTemplateMarkup();
-// movieQuickView.addMovieEventListener()
-
-createRow(localMovieDb.filter((ele) => {return ele.vote_average >= rating}),0,localGenreDb);
-
+createRow(localMovieDb.filter((ele) => {return Math.floor(ele.vote_average/2) >= rating}),0,localGenreDb);
+document.querySelectorAll('.menu__item ').forEach(item => item.classList.toggle('menu__item--active'))
 
 // createRow(localMovieDb,rating)
 function filterMovieData(){
     let userGivenValue = document.querySelector('input').value;
+    rating =rating_range.value;
     if(userGivenValue != ''){
-       
         let x = new Set()
          localGenreDb.forEach(ele =>{
             if( ele.name.toLowerCase().includes(userGivenValue.toLowerCase())){
@@ -42,27 +40,18 @@ function filterMovieData(){
         let z = [...y]
         filtererdData = localMovieDb.filter((ele) => {return ((ele.title.toLowerCase().includes(userGivenValue.toLowerCase()) 
                                                                || z.indexOf(ele) != -1)
-                                                               && ele.vote_average >= rating)
+                                                               && Math.floor(ele.vote_average/2) >= rating)
                                                     });
 
                                                     
         createRow(filtererdData,0,localGenreDb); 
     }
     else
-        createRow(localMovieDb.filter((ele) => {return ele.vote_average >= rating}),0,localGenreDb); 
+        createRow(localMovieDb.filter((ele) => {return Math.floor(ele.vote_average/2) >= rating}),0,localGenreDb); 
     
 }
 submitButton.addEventListener('click',filterMovieData);
-
-rating_range.addEventListener('change',()=>{
-    rating =rating_range.value;
-    if(filtererdData.length >=1){
-        createRow(filtererdData.filter((ele) => {return ele.vote_average >= rating}),0,localGenreDb);
-    }
-    else{
-        createRow(localMovieDb.filter((ele) => {return ele.vote_average >= rating}),0,localGenreDb);
-    }
-    
-});
+rating_range.addEventListener('change',filterMovieData);
 querryField.addEventListener('keyup',filterMovieData);
+
 movieQuickView.addMovieEventListener();
